125795d41664d500e2c571c2ddba76dc5a14c032
Rajat <joshirajat09@gmail.com>
2023-02-08 17:32:19 +0530

    Updating .gitignore file and Create Career Page function script

--- tests/testSuite.py
+++ tests/testSuite.py
@@ -4,7 +4,7 @@ from pages.base import verify_Create_Career_Page, verify_Posting_Job
 
 #1st Test Case for E2E Creating Career Page flow
 
-# verify_Create_Career_Page("https://www.expertia.ai/")
+verify_Create_Career_Page("https://www.expertia.ai/")
 
 #2nd Test Case for E2E Posting a Job\
 
